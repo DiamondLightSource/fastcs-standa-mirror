fdd0abe691266e34b75a2da5f4a514a01c8b6bde
Tom Kane <thomas.kane@diamond.ac.uk>
2025-12-16 15:15:25 +0000

    removed pvi setup code

--- src/fastcs_standa_mirror/__main__.py
+++ src/fastcs_standa_mirror/__main__.py
@@ -7,11 +7,7 @@ from fastcs.launch import FastCS
 from fastcs.transports.epics import EpicsGUIOptions, EpicsIOCOptions
 from fastcs.transports.epics.ca.transport import EpicsCATransport
 
-# from fastcs.transports.epics.pva.pvi import add_pvi_info
-# from fastcs.transports.epics.pva.transport import EpicsPVATransport
 from fastcs_standa_mirror.mirror_controller import MirrorController
-
-# import libximc.highlevel as ximc
 from fastcs_standa_mirror.utils import create_virtual_device_uris
 
 from . import __version__
@@ -48,26 +44,16 @@ def main() -> None:
         gui=gui_options, epicsca=EpicsIOCOptions(pv_prefix=pv_prefix)
     )
 
-    # epics_pva = EpicsPVATransport(epicspva=EpicsIOCOptions(pv_prefix=pv_prefix))
-
     if use_virtual:
         print("No physical device, creating virtual devices")
 
         uris = create_virtual_device_uris()
         controller = MirrorController(uris[0], uris[1])
 
-        # Create FastCS with both CA and PVA
-        fastcs = FastCS(controller, [epics_ca])  # , epics_pva])
-
-        # Add PVI information for Phoebus auto-generation
-        # add_pvi_info(
-        #     provider=epics_pva,
-        #     pv_prefix=pv_prefix,
-        #     controller_api=fastcs._controller,
-        #     description="Two-axis mirror controller for VISR project",
-        # )
+        fastcs = FastCS(controller, [epics_ca])
 
         fastcs.run()
+
     # else:
 
     #     devices = ximc.enumerate_devices(
